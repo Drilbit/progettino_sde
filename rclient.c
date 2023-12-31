7efd3ce35db0edd6f8c6a6ea4826d727023d751f
Andrea Paolini <andrea40paolini@gmail.com>
2023-12-31 10:57:55 +0100

    avanzamento

--- rclient.c
+++ rclient.c
@@ -5,66 +5,121 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+const int MAXLEN = 1024;
 
+int compress(){
+	//execlp("tar", "tar", "-c", "-v", "-z","-f", "nome archivio", buffer(cartella dove prendi i file), (char*) 0 );
+	//alternativa
+	//system("tar"); 
+	
+	//la creazione della cartella va nel main a priori perché 3 righe. con mkdir e permessi.
+}
 
-int main(int argc, char* argv[]){
+int add (char* percorso, int sk) {
+	int len = strlen (percorso);	//lunghezza nome file
+	int fd;			//file descriptor
+	int dim_file;	//dimensione del file
+	int rd;	//byte letti
+	struct stat st;	//struttura per lettura dati file
+	char buff[MAXLEN];	//buffer di appoggio
+
+	//CONTROLLO dei caratteri validi
+	for (int i = 0; i < len; i++) {
+		if ( (percorso[i] < 'a' || percorso[i] > 'z') 
+			&& (percorso[i] < 'A' || percorso[i] > 'Z')
+			&& (percorso[i] != '.') ) {
+			fprintf(stderr, "Errore! carattere '%s' non valido\n", percorso[i]);
+			return -1;	//carattere non valido	
+		}
+	}
+	
+	//APERTURA FILE
+	if ( (fd = open(percorso, O_RDONLY)) == -1 ) {
+		//errore
+		fprintf(stderr, "Errore! File inesistente\n");
+		return -1;
+	}
+
+	//LETTURA DIMENSIONE
+	if ( fstat(fd, &st) ) {
+		//errore
+		fprintf(stderr, "Errore! %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	dim_file = st.st_size;
+	printf("Dimensione file %d\n", dim_file);
+
+	//INVIO FILE AL SERVER
+	printf("Invio del file %s al server in corso...\n", percorso);
+	send(sk, percorso, len, 0);		//invia nome del file
+	while (read(fd, buff, MAXLEN)) {//invia file	
+		send(sk, buff, MAXLEN, 0);
+	}
+	printf("File inviato con successo\n");
+
+	return 0;
+}
+
+int controlc(int segnale){
+	//SE segnale segnale = SIGINT è come mandare comando quit. chiudo con una exit.
+}
+
+int main(int argc, char* argv[])
+{
 	int sd;
+	int type;
 	struct sockaddr_in sa;
 	in_addr_t address;
 	char* addr_str = "127.0.0.1";
 	int port_no = 10000;
-	int reuse_opt = 1;
-	int statussocket; //debug
-	statussocket = socket(AF_INET, SOCK_STREAM, 0);
-	sd = statussocket; //debug
-	printf("status socket: %d\n", statussocket); //debug
-	int statusinet; //debug
-	statusinet = inet_pton(AF_INET, addr_str, (void*)&address);
-	printf("status inet: %d\n", statusinet); //debug
+	sd = socket(AF_INET, SOCK_STREAM, 0);
+	inet_pton(AF_INET, addr_str, (void*)&address);
 	sa.sin_family = AF_INET;
 	sa.sin_port = htons(port_no);
 	sa.sin_addr.s_addr = address;
-	setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &reuse_opt, sizeof(int));
-	int statusbind; //debug
-	statusbind = bind(sd, (struct sockaddr*)&sa, sizeof(sa));
-	printf("status bind: %d\n", statusbind); //debug
 	
-	//parte funzione nardini
-	
-	const int MAXQUEUE = 10;
-	struct sockaddr_in client_addr;
-	socklen_t client_addr_len = sizeof(client_addr);
-	// char addr_str[INET_ADDRSTRLEN]; debug, se lasciato fa conflitto con il char* sopra e non capisco perché.
-	const int BUFFLEN = 4096;
-	char buff[BUFFLEN];
-	ssize_t rcvdbytes, sndbytes;
-	pid_t pid;
-	int statuslisten; //debug
-	statuslisten = listen(sd, MAXQUEUE);
-	printf("status listen: %d\n", statuslisten); //debug
-	while(1){
-		sd = accept(sd, (struct sockaddr*)&client_addr, &client_addr_len);
-		printf("status connessione: %d\n", sd); //debug
-		pid = fork();
-		printf("status pid: :%d\n", pid); //debug
-		if (pid < 0){
-			printf("errore");
-		} else if (pid == 0){
-			if(inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, addr_str, INET_ADDRSTRLEN) < 0 ){
-				printf("errore nella seconda inetntop");
-			}
-			printf("connesso con %s:%d\n", addr_str, ntohs(client_addr.sin_port));
-			printf("sonoqui"); //debug
-			while(1){
-				rcvdbytes = recv(sd, &buff, BUFFLEN, 0); //if di controllo se riesce a ricevere
-				buff[rcvdbytes] = '\0';
-				printf("ricevuto %s\n", buff);
-				sndbytes = send(sd, "OK", 2, 0); //if controllo se riesce a mandare
-				if (strcmp(buff, "exit") == 0){
-					break;
-				}
-			}
-		}		
+	char buff[MAXLEN];
+	int buff_len;
+	char resp[3];
+	ssize_t snd_bytes, rcvd_bytes;
+	char* arg;	//Argomento del comando letto da tastiera
+
+	printf("Connessione...\n");
+	if( ( connect(sd, (struct sockaddr*)&sa, sizeof(struct sockaddr_in)) ) >= 0 ) {
+		printf("connesso, pronto\n");
 	}
+    while(1)
+    {	
+		//LETTO COMANDO DA TASTIERA
+        fgets(buff, MAXLEN, stdin);
+        buff_len = strlen(buff);
+        
+		//SEPARA GLI ARGOMENTI	e TOGLIE  EVENTUALE '\n' A FONDO STRINGA
+		if ( (arg = strchr (buff, '\n')) != NULL ) {
+			*arg = '\0';	//rimuove '\n'	(uso arg come appoggio)
+		}
+		if ( (arg = strchr (buff, ' ')) != NULL ) {
+			*(arg++) = '\0';	//mette terminatore a metà per separare comando e argomento
+		}
+
+		//INVIO COMANDO A SERVER
+        snd_bytes = send(sd, buff, strlen(buff), 0);
+        printf("Inviati %d bytes\n", snd_bytes);
+
+		//RISPOSTA DA SERVER
+		rcvd_bytes = recv(sd, &resp, 2, 0);
+        resp[rcvd_bytes] = '\0';
+        printf("Ricevuta risposta '%s'\n", resp);
+
+		//ESECUZIONE COMANDO
+        if (strcmp(buff, "exit") == 0)
+            break;
+		else if (strcmp(buff, "add") == 0)
+			add(arg, sd);
+    }
+    
+    return EXIT_SUCCESS;
 }
--- rserver.c
+++ rserver.c
@@ -6,68 +6,111 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <signal.h>
+#include <fcntl.h>
 
+const char nome_cartella[] = "fileserver/";
+const char cartella[] = "mkdir -m 777 fileserver";
+
+int connessione(int sd){
+
+}
+
+int add (int sk) {
+    const int MAX_LEN = 1024;
+	char buff[MAX_LEN];
+    int fd; //file descriptor
+    mode_t permessi = (S_IRUSR || S_IWUSR || S_IXUSR
+                    || S_IRGRP || S_IWGRP || S_IXGRP
+                    || S_IROTH || S_IWOTH || S_IXOTH);
+    
+    int rcv;    //byte ricevuti
+    
+    //CREAZIONE FILE
+    strcpy(buff, nome_cartella);
+    printf("directory: %s\n", buff);
+    rcv = recv(sk, &( buff[strlen(nome_cartella)] ), MAX_LEN, 0);
+    buff[strlen(nome_cartella) + rcv] = '\0';
+    printf("nuovo file: %s\n", buff);
+    fd = open(buff, O_CREAT || O_RDONLY, permessi);
+    printf("file descriptor: %d\n", fd);
+    //se già file con stesso nome??
+
+    //COPIA CONTENUTI FILE
+    while ( (rcv = recv(sk, &buff, MAX_LEN, 0)) == MAX_LEN) {
+        write(fd, &buff, rcv);
+    }
+
+	return 0;
+}
 
 int main(int argc, char* argv[])
 {
-    int sd;
+	int creazionecartella = system(cartella);
+	if(creazionecartella == 0){
+		printf("cartella creata correttamente\n");
+	} else {
+		printf("errore creazione cartella \n");
+	}
+	
+    int socketdescriptor;
     int type;
-    struct sockaddr_in sa;
+    struct sockaddr_in server_address;
     in_addr_t address;
-    char* addrserv = "127.0.0.1";
-    int port_no = 10000;
+    char* indirizzo_server = "127.0.0.1";
+    int porta_numero = 10000;
     int reuse_opt = 1;
-    sd = socket(AF_INET, SOCK_STREAM, 0);
-    inet_pton(AF_INET, addrserv, (void*)&address);
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons(port_no);
-    sa.sin_addr.s_addr = address;
+    socketdescriptor = socket(AF_INET, SOCK_STREAM, 0);
+    inet_pton(AF_INET, indirizzo_server, (void*)&address);
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(porta_numero);
+    server_address.sin_addr.s_addr = address;
     if (type == SOCK_STREAM)
-        setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &reuse_opt, sizeof(int));
+        setsockopt(socketdescriptor, SOL_SOCKET, SO_REUSEADDR, &reuse_opt, sizeof(int));
 
-    bind(sd, (struct sockaddr*)&sa, sizeof(sa));
-    printf("Socket %d associato a %s:%d\n", sd, addrserv, port_no);
+    bind(socketdescriptor, (struct sockaddr*)&server_address, sizeof(server_address));
+    printf("Socket %d associato a %s:%d\n", socketdescriptor, indirizzo_server, porta_numero);
     const int MAXQUEUE = 10;   
     int conn_sd;
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
+    struct sockaddr_in client_address;
+    socklen_t client_addr_len = sizeof(client_address);
     char addr_str[INET_ADDRSTRLEN];
     
-    const int BUFFLEN = 1024;
+    const int BUFFLEN = 4096;
     char buff[BUFFLEN];
-    ssize_t rcvd_bytes, snd_bytes;
+    ssize_t byte_ricevuti, byte_mandare;
     
     pid_t pid;
 
-    listen(sd, MAXQUEUE);
+    listen(socketdescriptor, MAXQUEUE);
     while (1)
     {    
         printf("--- In attesa di connessione ---\n");
-        conn_sd = accept(sd, (struct sockaddr*)&client_addr, &client_addr_len);
+        conn_sd = accept(socketdescriptor, (struct sockaddr*)&client_address, &client_addr_len);
         if ((pid = fork()) < 0)
         {
             fprintf(stderr, "Impossibile creare un nuovo processo: %s\n", strerror(errno));
         }
         else if (pid == 0)
         {
-           inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, addr_str, INET_ADDRSTRLEN);
-            printf("Connesso col client %s:%d\n", addr_str, ntohs(client_addr.sin_port));
+            inet_ntop(AF_INET, &client_address.sin_addr.s_addr, addr_str, INET_ADDRSTRLEN);
+            printf("Connesso col client %s:%d\n", addr_str, ntohs(client_address.sin_port));
             while(1)
             {               
-                rcvd_bytes = recv(conn_sd, &buff, BUFFLEN, 0);             
-                buff[rcvd_bytes] = '\0';        
+                byte_ricevuti = recv(conn_sd, &buff, BUFFLEN, 0);             
+                buff[byte_ricevuti] = '\0';        
                 printf("Ricevuto '%s'\n", buff);        
-                snd_bytes = send(conn_sd, "OK", 2, 0);
+                byte_mandare = send(conn_sd, "OK", 2, 0);
                 printf("Risposta inviata\n");
                 if (strcmp(buff, "exit") == 0){
                     raise(SIGINT);
                     break;
                 }
+                else if (strcmp(buff, "add") == 0)
+			        add(conn_sd);
             }
             close(conn_sd);
             // termina il processo figlio
             exit(EXIT_SUCCESS);
-        } 
-    return EXIT_SUCCESS;
+        }
     }
 }
